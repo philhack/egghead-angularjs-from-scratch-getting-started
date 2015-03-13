041517a6129276487ac3924df80f371761c3c102
Phil Hack <phil@philhack.com>
2015-03-13 04:40:54 +0000

    added simple states for editing and creating

--- app/eggly-app.js
+++ app/eggly-app.js
@@ -21,15 +21,55 @@ angular.module('Eggly', [
             {"id":8, "title": "Dump", "url": "http://dump.com", "category": "Humor" }
         ];
 
+        $scope.isCreating = false;
+        $scope.isEditing = false;
         $scope.currentCategory = null;
         function setCurrentCategory(category){
             $scope.currentCategory = category;
+            cancelCreating();
+            cancelEditing();
         }
 
         function isCurrentCategory(category){
             return $scope.currentCategory != null && category.name === $scope.currentCategory.name;
         }
 
-        $scope.setCurrentCategory = setCurrentCategory;
         $scope.isCurrentCategory = isCurrentCategory;
+        $scope.setCurrentCategory = setCurrentCategory;
+
+
+        function startCreating(){
+            $scope.isCreating = true;
+            $scope.isEditing = false;
+        }
+
+        function cancelCreating(){
+            $scope.isCreating = false;
+        }
+
+        function startEditing(){
+            console.log('starting editing');
+            $scope.isCreating = false;
+            $scope.isEditing = true;
+        }
+
+        function cancelEditing(){
+            $scope.isEditing = false;
+        }
+
+        function shouldShowCreating(){
+            return $scope.currentCategory && !$scope.isEditing;
+        }
+
+        function shouldShowEditing(){
+            console.log('should show editing');
+            return $scope.isEditing && !$scope.isCreating;
+        }
+
+        $scope.startCreating = startCreating;
+        $scope.cancelCreating = cancelCreating;
+        $scope.startEditing = startEditing;
+        $scope.cancelEditing = cancelEditing;
+        $scope.shouldShowCreating = shouldShowCreating;
+        $scope.shouldShowEditing = shouldShowEditing;
     });
